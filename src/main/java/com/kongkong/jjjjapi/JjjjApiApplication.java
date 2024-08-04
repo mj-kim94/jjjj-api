81cb4ab710f0bf5565637de651e457dd63e0f202
gimminjeong <940313bay@naver.com>
2024-08-04 17:40:55 +0900

    init

---
+++ src/main/java/com/kongkong/jjjjapi/JjjjApiApplication.java
@@ -0,0 +1,13 @@
+package com.kongkong.jjjjapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjjjApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjjjApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kongkong/jjjjapi/JjjjApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kongkong.jjjjapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjjjApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
